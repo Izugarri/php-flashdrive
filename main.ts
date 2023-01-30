8119a620a6dca0de99168d2c201a6200afae4ff0
Izugarri <57817632+Izugarri@users.noreply.github.com>
2023-01-30 19:26:23 +0100

    Delete main.ts

--- main.ts
+++
@@ -1,9 +0,0 @@
-import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
-
-const app = new Application()
-
-var localPort = 80
-if (Deno.args.length > 0) {
-  localPort = parseInt(Deno.args[0])
-}
-app.static('/', '.').file('/', 'index.html').start({ port: localPort })
