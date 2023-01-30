c26ebc8980f1ee1de0b6b398ba6c249b18724c69
Izugarri <57817632+Izugarri@users.noreply.github.com>
2023-01-30 20:50:50 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 import { serve } from "https://deno.land/std/http/mod.ts";
-const reqHandler = async (req: Request) => {
-  const filePath = new URL(req.url).pathname;
-  return new Response(filePath);
-};
-serve(reqHandler, { port: 8080 });
+const app = new Application()
+
+var localPort = 80
+if (Deno.args.length > 0) {
+  localPort = parseInt(Deno.args[0])
+}
+app.static('/', '.').file('/', 'index.html').start({ port: localPort })
