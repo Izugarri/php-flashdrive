4d85f283e15f2b8abe00dbd2a2d9c1ef0b346056
Izugarri <57817632+Izugarri@users.noreply.github.com>
2023-01-30 19:50:43 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,36 @@
-import { Application } from 'https://deno.land/x/abc@v1.0.2/mod.ts'
+import { serve } from "https://deno.land/std@0.174.0/http/server.ts";
 
-const app = new Application()
+const server = Deno.listen({ port: 8080 });
+console.log("File server running on http://localhost:8080/");
 
-var localPort = 80
-if (Deno.args.length > 0) {
-  localPort = parseInt(Deno.args[0])
+for await (const conn of server) {
+  handleHttp(conn).catch(console.error);
 }
-app.static('/', '.').file('/', 'index.html').start({ port: localPort })
+
+async function handleHttp(conn: Deno.Conn) {
+  const httpConn = Deno.serveHttp(conn);
+  for await (const requestEvent of httpConn) {
+    // Use the request pathname as filepath
+    const url = new URL(requestEvent.request.url);
+    const filepath = decodeURIComponent(url.pathname);
+
+    // Try opening the file
+    let file;
+    try {
+      file = await Deno.open("." + filepath, { read: true });
+    } catch {
+      // If the file cannot be opened, return a "404 Not Found" response
+      const notFoundResponse = new Response("404 Not Found", { status: 404 });
+      await requestEvent.respondWith(notFoundResponse);
+      return;
+    }
+
+    // Build a readable stream so the file doesn't have to be fully loaded into
+    // memory while we send it
+    const readableStream = file.readable;
+
+    // Build and send the response
+    const response = new Response(readableStream);
+    await requestEvent.respondWith(response);
+  }
+}
