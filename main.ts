34e4c26bd3bc4888bba13e97f48d41b5e9ddb8a7
Izugarri <57817632+Izugarri@users.noreply.github.com>
2023-01-30 20:18:58 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 
 async function handler(_req) {
-  const path = await Deno.realPath("./index.html");
+  const path = await Deno.realPath("./");
 
-  return new Response(`The fully resolved path for ./index.html is ${path}`);
+  return new Response(`The fully resolved path for ./ ${path}`);
 }
 
 serve(handler);
