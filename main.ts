4005198d14a850ddf5045c19af8c81ef122804b0
Izugarri <57817632+Izugarri@users.noreply.github.com>
2023-01-30 20:03:22 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,36 +1,19 @@
-import { serve } from "https://deno.land/std@0.174.0/http/server.ts";
+/** @jsx h */
+import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+import { h } from "https://esm.sh/preact@10.5.15";
+import { renderToString } from "https://esm.sh/preact-render-to-string@5.1.19?deps=preact@10.5.15";
 
-const server = Deno.listen({ port: 8080 });
-console.log("File server running on http://localhost:8080/");
-
-for await (const conn of server) {
-  handleHttp(conn).catch(console.error);
+function handler(_req: Request): Response {
+  const page = (
+    <div>
+      <h1>Current time</h1>
+      <p>{new Date().toLocaleString()}</p>
+    </div>
+  );
+  const html = renderToString(page);
+  return new Response(html, {
+    headers: { "content-type": "text/html; charset=utf-8" },
+  });
 }
 
-async function handleHttp(conn: Deno.Conn) {
-  const httpConn = Deno.serveHttp(conn);
-  for await (const requestEvent of httpConn) {
-    // Use the request pathname as filepath
-    const url = new URL(requestEvent.request.url);
-    const filepath = decodeURIComponent(url.pathname);
-
-    // Try opening the file
-    let file;
-    try {
-      file = await Deno.open("." + filepath, { read: true });
-    } catch {
-      // If the file cannot be opened, return a "404 Not Found" response
-      const notFoundResponse = new Response("404 Not Found", { status: 404 });
-      await requestEvent.respondWith(notFoundResponse);
-      return;
-    }
-
-    // Build a readable stream so the file doesn't have to be fully loaded into
-    // memory while we send it
-    const readableStream = file.readable;
-
-    // Build and send the response
-    const response = new Response(readableStream);
-    await requestEvent.respondWith(response);
-  }
-}
+serve(handler);
