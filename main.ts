a341c3f1d297410e2029519c1548dc48ae338bf1
Izugarri <57817632+Izugarri@users.noreply.github.com>
2023-01-30 20:42:28 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,20 @@
 import { serve } from "https://deno.land/std/http/mod.ts";
-const BASE_PATH = "./";
+const BASE_PATH = "./public";
 const reqHandler = async (req: Request) => {
   const filePath = BASE_PATH + new URL(req.url).pathname;
-  return new Response(filePath);
+  let fileSize;
+  try {
+    fileSize = (await Deno.stat(filePath)).size;
+  } catch (e) {
+    if (e instanceof Deno.errors.NotFound) {
+      return new Response(null, { status: 404 });
+    }
+    return new Response(null, { status: 500 });
+  }
+  
+  const body = (await Deno.open(filePath)).readable;
+  return new Response(body, {
+    headers: { "content-length": fileSize.toString() },
+  });
 };
 serve(reqHandler, { port: 8080 });
