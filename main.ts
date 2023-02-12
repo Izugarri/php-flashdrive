8351aa3b82a660e597828219a1eabc12280cd994
Izugarri <57817632+Izugarri@users.noreply.github.com>
2023-02-12 19:04:22 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,9 @@
-import { serve } from "https://deno.land/std/http/mod.ts";
-import { lookup } from "https://deno.land/x/media_types/mod.ts";
+import { serve } from "https://deno.land/std@0.116.0/http/server.ts";
+import staticFiles from "https://deno.land/x/static_files@1.1.6/mod.ts";
 
-const BASE_PATH = "./public";
+const serveFiles = (req: Request) => staticFiles('public')({ 
+    request: req, 
+    respondWith: (r: Response) => r 
+})
 
-const reqHandler = async (req: Request) => {
-  const filePath = BASE_PATH + new URL(req.url).pathname;
-  let fileSize;
-  try {
-    fileSize = (await Deno.stat(filePath)).size;
-  } catch (e) {
-    if (e instanceof Deno.errors.NotFound) {
-      return new Response(null, { status: 404 });
-    }
-    return new Response(null, { status: 500 });
-  }
-  const body = (await Deno.open(filePath)).readable;
-  return new Response(body, {
-    headers: {
-      "content-length": fileSize.toString(),
-      "content-type": lookup(filePath) || "application/octet-stream",
-    },
-  });
-};
-
-serve(reqHandler, { port: 8080 });
+serve((req) => serveFiles(req), { addr: ':3000' });
