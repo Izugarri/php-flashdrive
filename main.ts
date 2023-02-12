891d29b66e811a273e70a24160a7e983795d572e
Izugarri <57817632+Izugarri@users.noreply.github.com>
2023-02-12 14:26:21 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,25 @@
-import { serve } from "https://deno.land/std/http/server.ts";
-const server = serve({ port: 5000 });
-for await (const server_request of server) {
-  const _file = await Deno.readFile('./index.html');
-  const decoder = new TextDecoder()
-  server_request.respond({ body: decoder.decode(_file) });
+import { serve, ServerRequest } from "https://deno.land/std@v0.5/http/server.ts";
+import * as expressive from "https://raw.githubusercontent.com/NMathar/deno-express/master/mod.ts";
+import { reset } from "https://raw.githubusercontent.com/denoland/deno_std/master/colors/mod";
+
+(async () => {
+  const port = 3000;
+  const app = new expressive.App();
+  app.use(expressive.simpleLog());
+  app.use(expressive.static_("./public"));
+  app.use(expressive.bodyParser.json());
+  app.get("/api/todos", async (req, res) => {
+    await res.json([{ name: "Buy some milk" }]);
+  });
+  // route with dynamic parameter
+  app.get("/api/user/{user_id}", async (req, res) => {
+    await res.json([{ id: req.params.user_id, name: "Jim Doe", phone: "12425323" }]);
+  });
+
+  // app.get('/', async (req, res) => {
+  //   await res.file('./index.html');
+  // });
+
+  const server = await app.listen(port);
+  console.log("app listening on port " + server.port);
+})();
