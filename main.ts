8bbf0a08ee174baca82d069ed0977517a57a266e
Izugarri <57817632+Izugarri@users.noreply.github.com>
2023-02-12 14:27:45 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,16 @@
-import { serve, ServerRequest } from "https://deno.land/std@v0.5/http/server.ts";
-import * as expressive from "https://raw.githubusercontent.com/NMathar/deno-express/master/mod.ts";
-import { reset } from "https://raw.githubusercontent.com/denoland/deno_std/master/colors/mod";
+import { Application } from "https://deno.land/x/oak/mod.ts";
 
-(async () => {
-  const port = 3000;
-  const app = new expressive.App();
-  app.use(expressive.simpleLog());
-  app.use(expressive.static_("./public"));
-  app.use(expressive.bodyParser.json());
-  app.get("/api/todos", async (req, res) => {
-    await res.json([{ name: "Buy some milk" }]);
-  });
-  // route with dynamic parameter
-  app.get("/api/user/{user_id}", async (req, res) => {
-    await res.json([{ id: req.params.user_id, name: "Jim Doe", phone: "12425323" }]);
-  });
+const app = new Application();
+app.use(async (ctx) => {
+  try {
+    await ctx.send({
+      root: `${Deno.cwd()}`,
+      
+    });
+  } catch {
+    ctx.response.status = 404;
+    ctx.response.body = "404 File not found";
+  }
+});
 
-  // app.get('/', async (req, res) => {
-  //   await res.file('./index.html');
-  // });
-
-  const server = await app.listen(port);
-  console.log("app listening on port " + server.port);
-})();
+await app.listen({ port: 8000 });
