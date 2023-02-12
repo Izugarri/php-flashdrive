c9c85480d47528de838b7264e91d8e556698229f
Izugarri <57817632+Izugarri@users.noreply.github.com>
2023-02-12 19:49:04 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
 import { serve } from "https://deno.land/std@0.116.0/http/server.ts";
 import staticFiles from "https://deno.land/x/static_files@1.1.6/mod.ts";
 
+function setHeaders(headers: Headers, path: string, stats?: Deno.FileInfo) {
+    headers.set("Content-disposition", "attachment; filename=" + path);
+}
 const serveFiles = (req: Request) => staticFiles('./')({ 
     request: req, 
     respondWith: (r: Response) => r 
 })
-
 serve((req) => serveFiles(req), { addr: ':3000' });
