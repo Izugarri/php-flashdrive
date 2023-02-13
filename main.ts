18d058fde9a47c6605841e8dade8a83c852bb53e
Izugarri <57817632+Izugarri@users.noreply.github.com>
2023-02-13 08:54:35 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { opine } from "https://deno.land/x/opine/mod.ts";
 import staticFiles from "https://deno.land/x/static_files@1.1.6/mod.ts";
 
-const app = new Application();
+const app = opine();
 
 app.use(staticFiles("./"));
 
-await app.listen({ port: 3000 });
+app.listen(3000);
